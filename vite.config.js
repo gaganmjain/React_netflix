152e9bcd983ac22c01beba8462749e5e89c7827c
Gagan m jain <gaganmjainwayanad@gmail.com>
2025-07-15 00:03:42 +0530

    Final update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_netflix/', 
+  base: process.env.VITE_BASE_PATH || "/React_netflix/", 
 });

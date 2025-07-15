2029dbde6e673539afe1f7830e495ca98a922f63
Gagan m jain <gaganmjainwayanad@gmail.com>
2025-07-15 12:21:40 +0530

    commited3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_netflix/', 
+  base: '/React_netflix/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
 });
